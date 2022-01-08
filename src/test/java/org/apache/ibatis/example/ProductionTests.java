ed5d13d1f82d06cd3fdcfda363b4b3ddeda7f7b6
zhouxinjian <zhouxinjian@xiaomi.com>
2022-01-08 18:59:53 +0800

    custome ProductionMapper

---
+++ src/test/java/org/apache/ibatis/example/ProductionTests.java
@@ -0,0 +1,47 @@
+/*
+ *    Copyright 2009-2022 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.example;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Williami
+ * @description
+ * @date 2022/1/8
+ */
+public class ProductionTests {
+
+
+  @DisplayName("测试自定义ProductionMapper")
+  @Test
+  public void testCustomProductionMapper() throws IOException {
+    String resource = "mybatis-config.xml";
+    InputStream inputStream = Resources.getResourceAsStream(resource);
+    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    Object selectOne = sqlSession.selectOne("org.mybatis.example.ProductionMapper.selectOne", 1L);
+  }
+
+
+}
